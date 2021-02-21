8e78651a62faeaa3e809f7aa06f761b1cb0d1813
abeidahmed <abeidahmed92@gmail.com>
2021-02-21 19:13:13 +0530

    Remove boilerplate test

--- spec/rails_heroicon_spec.rb
+++ spec/rails_heroicon_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe RailsHeroicon do
   it "has a version number" do
     expect(RailsHeroicon::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
