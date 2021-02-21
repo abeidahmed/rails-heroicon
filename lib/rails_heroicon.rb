bf02bbc6ccac331bbdd7641f2100b76f346c1d37
abeidahmed <abeidahmed92@gmail.com>
2021-02-21 19:56:22 +0530

    Join file path

--- lib/rails_heroicon.rb
+++ lib/rails_heroicon.rb
@@ -1,5 +1,7 @@
 require_relative "rails_heroicon/version"
+require_relative "rails_heroicon/rails_heroicon"
 
 module RailsHeroicon
-  class Error < StandardError; end
+  SOLID_ICON_PATH = File.join(File.dirname(__FILE__), "../icons/solid")
+  OUTLINE_ICON_PATH = File.join(File.dirname(__FILE__), "../icons/outline")
 end
