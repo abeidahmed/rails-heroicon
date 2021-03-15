eeb1c6339c059bb9b8fbd1ee5d68f106594bf5ca
abeidahmed <abeidahmed92@gmail.com>
2021-03-15 18:17:00 +0530

    Improve rdoc

--- lib/rails_heroicon/helper.rb
+++ lib/rails_heroicon/helper.rb
@@ -3,18 +3,14 @@ require "action_view"
 module RailsHeroicon
   module Helper
     # To add a heroicon, call <tt><%= heroicon "icon_name" %></tt> on your erb template.
-    # Head over to https://heroicons.com/ to view all the icons.
+    # Head over to https://heroicons.com to view all the icons.
     #
     # == Options
     # The helper method accepts mutiple arguments such as:
     #
     # === Variant
     # There are two types of variants: 'outline' and 'solid', the default being the 'outline'.
-    # To specify the solid variant, call <tt><%= heroicon "icon_name", variant: :solid %></tt>
-    #
-    # === Accessibility
-    # The helper method automatically sets <tt>aria-hidden=true</tt> if <tt>aria-label</tt> is not set, and
-    # if <tt>aria-label</tt> is set, then <tt>role=img</tt> is set automatically.
+    # To specify the solid variant, call <tt><%= heroicon "icon_name", variant: "solid" %></tt>
     #
     # === HTML attributes
     # Any <tt>html</tt> attribute is supported, for eg:
@@ -32,6 +28,10 @@ module RailsHeroicon
     # If the variant is set as <tt>outline</tt>, size automatically defaults to 24, and if the variant is set as
     # <tt>solid</tt>, size automatically defaults to 20. However, this can be over-written with the <tt>size</tt>
     # attribute.
+    #
+    # == Accessibility
+    # The helper method automatically sets <tt>aria-hidden=true</tt> if <tt>aria-label</tt> is not set, and
+    # if <tt>aria-label</tt> is set, then <tt>role=img</tt> is set automatically.
     def heroicon(symbol, **options)
       icon = RailsHeroicon.new(symbol, **options)
       content_tag(:svg, icon.svg_path.html_safe, icon.options)
