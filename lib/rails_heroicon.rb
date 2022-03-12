6270f94292d8d8e615d9d40681c71d4bf642eb66
abeidahmed <abeidahmed92@gmail.com>
2022-03-12 15:04:07 +0530

    feat: support ruby 2.6+

--- lib/rails_heroicon.rb
+++ lib/rails_heroicon.rb
@@ -4,6 +4,6 @@ require_relative "rails_heroicon/rails_heroicon"
 require_relative "rails_heroicon/railtie" if defined? Rails
 
 module RailsHeroicon
-  ICON_PATH = File.join(File.dirname(__FILE__), "..", "compressed", "icons.json")
-  ICONS = JSON.load_file(ICON_PATH)
+  ICON_PATH = File.join(File.dirname(__FILE__), "../compressed/icons.json")
+  ICONS = JSON.parse(File.read(ICON_PATH)).freeze
 end
